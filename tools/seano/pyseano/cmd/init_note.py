f36dd823d20e497faa96301643de8f7e879eb44e
Andrew Keller <andrew@kellerfarm.com>
2020-01-07 14:19:45 -0500

    fix: Enable projects to suppliment the note template rather than overwrite it
    This commit exists for posterity only; data unrelated to the history of seano has been removed.
    Original repository: zarf.git
    Original commit ID: 9ce1909b016c87a36427f2de3af16ea7c81b024c

--- tools/seano/pyseano/cmd/init_note.py
+++ tools/seano/pyseano/cmd/init_note.py
@@ -6,7 +6,12 @@ Interactive command-line wrapper on top of the infrastructure that creates a new
 
 from pyseano.db import *
 from pyseano.utils import *
+import sys
 
 
 def make_new_release_notes(db, count):
     edit_files(open_seano_database(db).make_new_notes(count))
+
+
+def print_note_template(db):
+    sys.stdout.write(open_seano_database(db).get_seano_note_template_contents())
