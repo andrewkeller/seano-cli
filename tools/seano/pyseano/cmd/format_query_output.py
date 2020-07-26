77ba5fded86899a551089ad582c0a1228cc6cc0d
Andrew Keller <andrew@kellerfarm.com>
2020-07-26 12:50:50 -0400

    fix: Add command-line access to Zarf's seano view renderers
    Convenient for debugging
    
    This commit exists for posterity only; data unrelated to the history of seano has been removed.
    Original repository: zarf.git
    Original commit ID: fa4693dc88fa027d2d0d61bb52d47070c4738cf4

---
+++ tools/seano/pyseano/cmd/format_query_output.py
@@ -0,0 +1,52 @@
+"""
+pyseano/cmd/format_query_output.py
+
+Interactive command-line wrapper on top of the infrastructure that converts a seano query output file into
+a human-readable format.
+"""
+
+# Gain access to Zarf's Python support module, which is where the stock seano views are located:
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..', '..', '..', '..')))
+
+import logging
+from support.seano.views.qa_notes import compile_qa_notes
+
+log = logging.getLogger(__name__)
+
+format_functions = {
+    'qa-notes': compile_qa_notes,
+}
+
+format_choices = list(format_functions.keys());
+
+
+def format_query_output(src, format, out):
+
+    if not src:
+        log.error('Invalid seano query output file: (empty string)')
+        sys.exit(1)
+
+    if not out:
+        log.error('Invalid destination file: (empty string)')
+        sys.exit(1)
+
+    if format not in format_functions:
+        log.error('Invalid format "%s": not supported', format)
+        sys.exit(1)
+
+    if src in ['-']:
+        srcdata = sys.stdin.read()
+    else:
+        with open(src, 'r') as f:
+            srcdata = f.read()
+
+    outdata = format_functions[format](srcdata)
+
+    if out in ['-']:
+        sys.stdout.write(outdata)
+        return
+
+    with open(out, 'w') as f:
+        f.write(outdata)
