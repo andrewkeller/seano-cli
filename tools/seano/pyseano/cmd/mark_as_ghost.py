a5e374ca0866acc97ce4ec0c3ecf53146886616d
Andrew Keller <andrew@kellerfarm.com>
2022-11-08 14:28:48 -0500

    fix: Add the ability to ghost a single note at a time
    This commit exists for posterity only; data unrelated to the history of seano has been removed.
    Original repository: zarf.git
    Original commit ID: 3307057c0189fc120b600472b33820061d4f8d74

--- tools/seano/pyseano/cmd/mark_as_ghost.py
+++ tools/seano/pyseano/cmd/mark_as_ghost.py
@@ -6,14 +6,40 @@ currently existing extern notes as ghosts.
 """
 
 from pyseano.db import *
+from pyseano.utils import *
 
-import os
-import sys
+log = logging.getLogger(__name__)
 
 
-def mark_as_ghost(db_search_seed_path, is_dry_run, extern_id):
+def mark_as_ghost(db_search_seed_path, is_dry_run, extern_ids, patterns):
     db = find_and_open_seano_database(db_search_seed_path)
-    path_info = db.mark_all_notes_as_ghosts(is_dry_run=is_dry_run, extern_id=extern_id)
+    files = []
+    # IMPROVE: In a Git-backed project, `most_recently_added_notes()` and
+    #          `get_notes_matching_pattern()` are both implemented using the
+    #          Git scanner.  And here, our use of `get_notes_matching_pattern()`
+    #          is inside a for loop.  There's some room for improvement here
+    #          with regard to performance; although the Git scanner is designed
+    #          to be as fast as possible, it is also not terribly cheap; it
+    #          would be nice to invoke it less often.
+    for extern_id in extern_ids:
+        files.extend(db.get_notes_with_extern_id(extern_id))
+        log.debug("Found notes with extern ID '%s':\n    %s", extern_id, "\n    ".join(files))
+
+    for pattern in patterns:
+        new_files, errors = db.get_notes_matching_pattern(pattern, include_modified=True)
+        if not new_files:
+            raise SeanoFatalError('Unable to resolve pattern:\n    %s' % ('\n    '.join(errors),))
+        log.debug("Pattern '%s' yielded:\n    %s", pattern, "\n    ".join(new_files))
+        files.extend(new_files)
+
+    if not files:
+        raise SeanoFatalError('Unable to identify any notes that match your query (add -v to troubleshoot)')
+
+    files = sorted(set(files))
+    log.debug("About to ghost:\n    %s", "\n    ".join(files))
+
+    path_info = [db.ghost_note(note_file=file, is_dry_run=is_dry_run) for file in files]
+    path_info = [x for x in path_info if x]
 
     if path_info:
         sys.stderr.write('\n'.join([
@@ -24,3 +50,5 @@ def mark_as_ghost(db_search_seed_path, is_dry_run, extern_id):
             'Please review the changes and commit them.',
             '',
         ]))
+    else:
+        sys.stderr.write('No changes were made to any selected notes\n')
