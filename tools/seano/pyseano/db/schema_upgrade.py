af406f6b5da50dfa3ed6478ddc301e57ace05b02
Andrew Keller <andrew@kellerfarm.com>
2023-08-29 17:08:17 -0400

    feat: Add support for Markdown in `seano` views
    This commit exists for posterity only; data unrelated to the history of seano has been removed.
    Original repository: zarf.git
    Original commit ID: b8ef010cae1b8156b539ee0a3c565d243637fa3b

--- tools/seano/pyseano/db/schema_upgrade.py
+++ tools/seano/pyseano/db/schema_upgrade.py
@@ -19,67 +19,6 @@ def validate_is_string_or_none(value):
     raise SeanoFatalError('expected string or None but found %s' % (value,))
 
 
-def convert_hlist_rst_to_rst(value):
-    '''
-    If the given value looks like rst, then the given value is returned as-is.
-
-    Else, if the given value looks like hlist-rst, then the value is converted
-    to flat rst, and the result is returned.
-
-    Else, an exception is raised.
-
-    ABK: Refer to the usage of this function for an architecture discussion.
-    '''
-    if isinstance(value, (ascii_str_type, unicode_str_type)):
-        # This value is already a flat string (not an hlist).  Return as-is.
-        return value
-    if isinstance(value, list):
-        # This value is an hlist of some kind.  Flatten it into a string, assuming
-        # that the rich text markup being used is reStructuredText.
-
-        # The top layer of hlist elements become paragraphs in reStructuredText,
-        # and the next layer in becomes the new root of newer, smaller hierarchical
-        # lists, formatted in reStructuredText.
-
-        def flatten(v, level=0):
-            if not v:
-                yield level, None
-                return
-            if isinstance(v, (ascii_str_type, unicode_str_type)):
-                yield level, v
-                return
-            if isinstance(v, list):
-                for e in v:
-                    for x in flatten(e, level):
-                        yield x
-                return
-            if isinstance(v, dict):
-                for head, inner in v.items():
-                    yield level, head
-                    for x in flatten(inner, level + 1):
-                        yield x
-                return
-            raise SeanoFatalError('unsupported hlist value: %s' % (v,))
-
-        def serialize():
-            previousLevel = None
-            for level, txt in flatten(value):
-                if not txt:
-                    continue
-                if not level or level != previousLevel:
-                    yield '\n'
-                previousLevel = level
-                if level:
-                    yield '  ' * (level -1)
-                    yield '* '
-                yield txt
-                yield '\n'
-
-        return ''.join(serialize()).strip()
-
-    raise SeanoFatalError('expected hlist or string but found %s' % (value,))
-
-
 def upgrade_note_schema(key, value):
     if key in ['commits', 'releases', 'tickets']:
         if not value:
@@ -98,31 +37,6 @@ def upgrade_note_schema(key, value):
             # This is the correct, modern type, but we still have to validate the contents.
             return set([validate_is_string_or_none(x) for x in value])
         raise SeanoFatalError('unsupported data type for %s list: %s' % (key, value))
-    if key.endswith('-loc-rst') and isinstance(value, dict):
-        # According to the name of this field, this field is supposed to have a loc-rst
-        # value.  For developer convenience, we also permit loc-hlist-rst values here;
-        # such values are auto-converted to loc-rst on-the-fly by the schema upgrader
-        # (this infrastructure).  By the time this value gets to the seano query dump
-        # file (the main output of the seano executable -- the data consumed by views),
-        # the value has already been migrated, and nobody will have any clue that the
-        # original data type was loc-hlist-rst.
-        #
-        # ABK: I have ethical qualms with teaching seano things about markup.  The way I
-        # see it, there are 2 main options:
-        #
-        # - teach seano to auto-migrate loc-hlist-rst values to loc-rst when appropriate
-        # - teach the views that loc-rst values might contain loc-hlist-rst data
-        #
-        # The former is bad because seano should not be responsible for markup in any
-        # way.  The latter is bad because violating type patterns only leads to more
-        # complexity, and the views are already complicated enough.
-        #
-        # For the time being, I believe the lesser of those evils is the former -- that
-        # seano should auto-migrate the schema at query time -- and that the views should
-        # be able to trust advertised data types, and be oblivious to this problem.
-        #
-        # Iterate as needed.
-        return {loc: convert_hlist_rst_to_rst(val) for loc, val in value.items()}
     return value
 
 
